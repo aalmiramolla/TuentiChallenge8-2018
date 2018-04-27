12b619eae27251dc02c0cf9431c4b3180b7a664f
Alejandro Almira <aalmiramolla@gmail.com>
2018-04-27 19:49:09 +0200

    Fixed challenge1 folder and try of challenge 3

--- challenge1/challenge1.py
+++ challenge1/challenge1.py
@@ -7,9 +7,9 @@ def how_many_squares(line):
 
 if __name__=='__main__':
     test_or_submit = 'submit'
-    with open('challenge1/'+test_or_submit+'Input') as f:
+    with open(test_or_submit+'Input') as f:
         lines = f.readlines()
         c = map(how_many_squares, lines[1:])
-        with open('challenge1/'+test_or_submit+'Output', 'w') as r:
+        with open(test_or_submit+'Output', 'w') as r:
             for idx, value in enumerate(c):
                   r.write('Case #{}: {}\n'.format(idx+1,value))
